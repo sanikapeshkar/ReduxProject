ae890276b51165d32c11b883fecacb6b52c12791
Sanika Peshkar <sanikapeshkar480@gmail.com>
2025-04-13 17:41:41 +0530

    feat : [Issue Board] : Created Issue Board Header

---
+++ Frontend/src/components/IssueBoard/IssueBoard.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import IssueBoardHeader from "../IssueBoardHeader/IssueBoardHeader";
+
+function IssueBoard() {
+  return (
+    <div>
+      <IssueBoardHeader />
+    </div>
+  );
+}
+
+export default IssueBoard;
