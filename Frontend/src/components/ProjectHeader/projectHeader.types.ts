4ea029add23c6d85e0dbd4b73ebc88b51204af15
Sanika Peshkar <sanikapeshkar480@gmail.com>
2025-06-24 22:54:51 +0530

    fix: build

--- Frontend/src/components/ProjectHeader/projectHeader.types.ts
+++ Frontend/src/components/ProjectHeader/projectHeader.types.ts
@@ -1,8 +1,7 @@
-import { Dispatch, SetStateAction } from "react"
 
 export type ProjectHeaderProps = {
   selectedProject: string
-  setSelectedProject: Dispatch<SetStateAction<"project-1" | "project-2" | "project-3">>;
+  setSelectedProject: (val: string) => void;
   timeRange: string
   setTimeRange: (val: string) => void
 }
